25e384adfac63cdb9e9d971954df137ccd49435f
Aamir.Ameer <aamiraali1125@gmail.com>
2022-04-04 15:06:14 +0500

    test

--- src/main/java/com/mywebapp/MywebappApplication.java
+++ src/main/java/com/mywebapp/MywebappApplication.java
@@ -2,12 +2,22 @@ package com.mywebapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class MywebappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MywebappApplication.class, args);
+	}
+	
+	@GetMapping("/")
+	@RequestMapping 
+	public String index() {
+		return "message from webapp";
 	}
 
 }
